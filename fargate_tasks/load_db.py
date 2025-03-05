87f0808d314cf2a6c91b05d1c09726c62a7308a1
Pranav Chaturvedi <pranavhfs1@gmail.com>
2025-03-05 22:07:54 +0530

    Daily ID Export Integration

--- fargate_tasks/load_db.py
+++ fargate_tasks/load_db.py
@@ -3,8 +3,12 @@ import boto3
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.dialects import postgresql as pg
-import pandas as pd
 import requests
+import gzip
+import json
+from concurrent.futures import ThreadPoolExecutor
+import datetime
+from io import BytesIO
 import time
 
 load_dotenv(".env")
@@ -13,9 +17,6 @@ load_dotenv(".env")
 # Getting Supbase and TMDB Variables from SSM
 
 ssm_client = boto3.client("ssm")
-s3_client = boto3.client("s3")
-films_backup_file = "films_backup.csv"
-series_backup_file = "series_backup.csv"
 
 supabase_db_result = ssm_client.get_parameters_by_path(
     Path="/supabase/",
@@ -24,28 +25,12 @@ supabase_db_result = ssm_client.get_parameters_by_path(
 tmdb_result = ssm_client.get_parameters_by_path(
     Path="/tmdb/", Recursive=True, WithDecryption=True
 )
-s3_backup_bucket = (
-    ssm_client.get_parameter(Name="/imdb/backup/bucket")
-    .get("Parameter", {})
-    .get("Value", None)
-)
 supabase_db_params = dict()
 tmdb_api_params = dict()
 for param in supabase_db_result.get("Parameters", []):
     supabase_db_params[param["ARN"].split("/")[-1]] = param["Value"]
 for param in tmdb_result.get("Parameters"):
     tmdb_api_params[param["ARN"].split("/")[-1]] = param["Value"]
-
-# Getting backup csv files from S3
-
-if s3_backup_bucket:
-    s3_client.download_file(s3_backup_bucket, films_backup_file, films_backup_file)
-    s3_client.download_file(s3_backup_bucket, series_backup_file, series_backup_file)
-
-# Getting movie and TV Show DF
-
-films_df = pd.read_csv(films_backup_file)
-series_df = pd.read_csv(series_backup_file)
 
 # Creating engine
 
@@ -55,39 +40,30 @@ supabase_engine = create_engine(connection_string)
 
 # Create tables if not created already
 
-with supabase_engine.begin() as connection:
-    print("Connection has been established to Supabase")
-    metadata.create_all(bind=connection, checkfirst=True)
 
-    films_imdb_ids = films_df["IMDB_ID"].tolist()
-    print(f"Adding Data for {len(films_imdb_ids)} films.")
-    count = 0
-    for imdb_id in films_imdb_ids:
-        api_url = f"https://{tmdb_api_params['path']}/3/find/{imdb_id}?api_key={tmdb_api_params['key']}&external_source=imdb_id"
-        response = requests.get(api_url).json()
-
-        if len(response.get("movie_results",[])) <= 0:
-            continue
-        movie_detail = response["movie_results"][0]
-        id = movie_detail["id"]
+def add_media_into_supbase(tmdb_id, media_type):
+    with supabase_engine.begin() as connection:
+        api_url = f"https://{tmdb_api_params['path']}/3/{media_type}/{tmdb_id}?api_key={tmdb_api_params['key']}"
+        response = requests.get(api_url, timeout=(30, 30))
+        if response.status_code != 200:
+            return
+        movie_detail = response.json()
+        if not movie_detail.get("imdb_id"):
+            return
         entry = {
-            "type": "movie",
+            "type": media_type,
             "backdrop_path": movie_detail["backdrop_path"],
             "poster_path": movie_detail["poster_path"],
             "original_language": movie_detail["original_language"],
             "release_date": movie_detail["release_date"],
-            "imdb_id": imdb_id,
+            "imdb_id": movie_detail["imdb_id"],
             "title": movie_detail["title"],
+            "homepage": movie_detail["homepage"],
             "overview": movie_detail["overview"],
+            "status": movie_detail["status"],
+            "tagline": movie_detail["tagline"],
+            "genre": [i["name"] for i in movie_detail["genres"]],
         }
-        time.sleep(2)
-
-        misc_query_url = f"https://{tmdb_api_params['path']}/3/movie/{id}?api_key={tmdb_api_params['key']}"
-        misc_response = requests.get(misc_query_url).json()
-        entry["tagline"] = misc_response["tagline"]
-        entry["homepage"] = misc_response["homepage"]
-        entry["status"] = misc_response["status"]
-        entry["genre"] = [i["name"] for i in misc_response["genres"]]
 
         # make it insert into update
         connection.execute(
@@ -97,11 +73,42 @@ with supabase_engine.begin() as connection:
                 index_elements=[SeenFlixAggregated.c.imdb_id], set_=entry
             )
         )
-        count += 1
-        if count % 500 == 0:
-            print(f"Added {count} movies")
+        connection.commit()
+        time.sleep(1)
 
 
-# Loop through Movies and Add inside SeenFlix aggregated (insert on conflict do upadate)
+with supabase_engine.begin() as connection:
+    print("Connection has been established to Supabase")
+    metadata.create_all(bind=connection, checkfirst=True)
 
-# Loop through Shows DF and add inside SeenFlixAggregated and Episode Count
+# Movies addition
+
+tmdb_ids = []
+two_days_ago_datetime = datetime.datetime.today() - datetime.timedelta(days=2)
+two_days_ago_datetime = two_days_ago_datetime.date().strftime("%m_%d_%Y")
+
+movie_file_name = f"movie_ids_{two_days_ago_datetime}.json.gz"
+download_movie_gzip_url = f"http://files.tmdb.org/p/exports/{movie_file_name}"
+response = requests.get(download_movie_gzip_url, allow_redirects=True)
+
+movie_tmdb_objects = []
+with gzip.GzipFile(fileobj=BytesIO(response.content)) as gz:
+    for line in gz:
+        movie_tmdb_objects.append(json.loads(line.decode()))
+    movie_tmdb_ids = [obj["id"] for obj in movie_tmdb_objects]
+print(f"Adding Data for {len(movie_tmdb_ids)} films.")
+
+worker_inputs = list(
+    zip(
+        movie_tmdb_ids,
+        ["movie"] * len(movie_tmdb_ids),
+    )
+)
+
+# with ThreadPoolExecutor(max_workers=25) as executor:
+# executor.map(lambda input : add_media_into_supbase(input[0],input[1]), worker_inputs)
+
+for input in worker_inputs[:500]:
+    add_media_into_supbase(input[0], input[1])
+
+# TV Series Addition
