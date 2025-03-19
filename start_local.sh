6874b09db7a9f00a8792e0a720af608abf22c62d
Pranav Chaturvedi <pranavhfs1@gmail.com>
2025-03-19 21:40:36 +0530

    firgured out packages in lambda. now ssm in common.py is broken

--- start_local.sh
+++ start_local.sh
@@ -1,1 +1,3 @@
-serverless offline
+/bin/bash uv export --frozen > requirements.txt
+/bin/bash uv pip install -r requirements.txt --target . --python-platform x86_64-manylinux_2_17
+/bin/bash rm requirements.txt
